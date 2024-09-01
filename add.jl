de5b257e4bd7cd6bd3f951ef4a386a23ad700769
Phuoc Nguyen <phuoc.nguyen@deakin.edu.au>
2024-09-01 22:58:48 +1000

    i7

--- add.jl
+++ add.jl
@@ -1,26 +1,2 @@
 ]
-add BenchmarkTools BSON ChainRules ChainRulesCore CSV DataFrames Distributions EndpointRanges FileIO Flux Functors ImageFiltering Images JLD2 LinearAlgebra MacroTools MLDatasets MLUtils NNlib OneHotArrays Parameters Plots ProgressMeter Random Zygote
-]add PythonCall
-
-using PythonCall
-using Parameters: @unpack
-
-using Revise
-using DataFrames, Distributions, BayesNets, CSV, Tables
-using BayesNets: plot, name
-using DataFrames: index
-using Graphs, GraphPlot
-using Revise
-using DataFrames, Distributions, BayesNets, CSV, Tables, FileIO, JLD2
-using Optimisers, BSON
-using ProgressMeter: Progress, next!
-using CUDA
-using Flux
-using Flux: gpu, Chain, Dense, relu, DataLoader
-using ParameterSchedulers
-using ParameterSchedulers: Scheduler, Stateful, next!
-using Optimisers: Descent, adjust!
-
-using Distributions: sample, mean, std
-using DataFrames
-using StatsBase
+add BayesNets BenchmarkTools BSON ChainRules ChainRulesCore CSV CUDA DataFrames Distributions EndpointRanges FileIO Flux Functors GraphPlot Graphs ImageFiltering Images JLD2 LinearAlgebra MacroTools MLDatasets MLUtils NNlib OneHotArrays Optimisers Parameters ParameterSchedulers Plots ProgressMeter PythonCall Random Revise StatsBase Tables Zygote
