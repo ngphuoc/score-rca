3bcf66a87c77c98e481c3c3a71a8112780de149f
Phuoc Nguyen <phuoc.nguyen@deakin.edu.au>
2024-09-02 16:13:44 +1000

    Switch to randomfourier features

--- main-generated-2d-v1.jl
+++ main-generated-2d-v1.jl
@@ -106,12 +106,9 @@ function Diffusion2d(; args)
     @assert args.input_dim == 2
     X, H, E, n_timesteps = (2, args.hidden_dim, args.embed_dim, args.n_timesteps)
     model = ConditionalChain(
-                             Parallel(.+, Dense(2, H), Chain(RoundTimesteps(n_timesteps), SinusoidalPositionEmbedding(n_timesteps, H), Dense(H, H))),
-                             swish,
-                             Parallel(.+, Dense(H, H), Chain(RoundTimesteps(n_timesteps), SinusoidalPositionEmbedding(n_timesteps, H), Dense(H, H))),
-                             swish,
-                             Parallel(.+, Dense(H, H), Chain(RoundTimesteps(n_timesteps), SinusoidalPositionEmbedding(n_timesteps, H), Dense(H, H))),
-                             swish,
+                             Parallel(.+, Dense(2, H), Chain(RandomFourierFeatures(H, fourier_scale), Dense(H, H))), swish,
+                             Parallel(.+, Dense(H, H), Chain(RandomFourierFeatures(H, fourier_scale), Dense(H, H))), swish,
+                             Parallel(.+, Dense(H, H), Chain(RandomFourierFeatures(H, fourier_scale), Dense(H, H))), swish,
                              Dense(H, 2),
                             )
     return Diffusion2d(σ_max, model)
@@ -147,4 +144,45 @@ for epoch = 1:args.epochs
     end
     next!(progress; showvalues=[(:loss, total_loss/length(loader))])
 end
+
+@info "Plots"
+include("plot-joint.jl")
+xlim = ylim = (-5, 5)
+
+#-- defaults
+default(; fontfamily="Computer Modern", titlefontsize=14, linewidth=2, framestyle=:box, label=nothing, aspect_ratio=:equal, grid=true, xlim, ylim, color=:seaborn_deep, markersize=2, leg=nothing)
+
+#-- plot data
+x, y = eachcol(normal_df)
+pl_data = scatter(x, y; xlab=L"x", ylab=L"y", title=L"Data $(x, y)$")
+
+#-- plot perturbations
+x, y = eachcol(perturbed_df)
+pl_perturbed_data = scatter(x, y; xlab=L"x", ylab=L"y", title=L"Perturbed $3\sigma$ $(x, y)$")
+
+x = @> normal_df Array transpose Array gpu;
+d = size(x, 1)
+X, batchsize = size(x, 1), size(x)[end]
+t = rand!(similar(x, size(x)[end])) .* (1f0 - 1f-5) .+ 1f-5  # same t for j and paj
+σ_t = expand_dims(marginal_prob_std(t; args.σ_max), 1)
+z = 2rand!(similar(x)) .- 1;
+x̃ = x .+ σ_t .* z
+
+x, y = eachrow(cpu(x̃))
+pl_sm_data = scatter(x, y; xlab=L"x", ylab=L"y", title="Perturbed score matching")
+
+#-- plot gradients
+x = @> Iterators.product(range(xlim..., length=20), range(ylim..., length=20)) collect vec;
+x = @> reinterpret(reshape, Float64, x) Array{Float32} gpu;
+d = size(x, 1)
+t = fill!(similar(x, size(x)[end]), 0.1) .* (1f0 - 1f-5) .+ 1f-5  # same t for j and paj
+σ_t = expand_dims(marginal_prob_std(t; args.σ_max), 1)
+J = @> unet(x, t)
+@≥ J, x cpu.()
+x, y = eachrow(x);
+u, v = eachrow(0.2J);
+pl_gradient = scatter(x, y, markersize=0, lw=0, color=:white);
+arrow0!.(x, y, u, v; as=0.2, lw=1.0);
+
+@> Plots.plot(pl_data, pl_perturbed_data, pl_sm_data, pl_gradient; xlim, ylim, size=(1000, 800)) savefig("fig/$datetime_prefix-2d.png")
 
