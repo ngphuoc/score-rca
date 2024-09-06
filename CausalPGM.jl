58ca0ff9fdb0f211da0ea339983142bdee8f9e8b
Phuoc Nguyen <phuoc.nguyen@deakin.edu.au>
2024-09-06 23:24:32 +1000

    unpack GroupDense

--- CausalPGM.jl
+++ CausalPGM.jl
@@ -17,18 +17,6 @@ Optimisers.trainable(model::CausalPGM) = (; model.fs)
 #     CausalPGM(dag, ps, fs)
 # end
 
-""" Sample zero mean noises from CausalPGM.ps """
-function sample_noise(model::CausalPGM, n_samples::Int)
-    @> rand.(model.ps, n_samples) transpose.() vcats
-end
-
-""" One-step mapping throught the model, return the location """
-function (model::CausalPGM)(x::AbstractVecOrMat{<:Real})
-    @unpack dag, ps, fs = model
-    xb = @> unsqueeze(x, 2) repeat(outer=(1, size(dag, 1)))
-    yb = @> fs(xb) squeeze(1)
-end
-
 """ For @assertion in forward mode to check if weights have been masked or not"""
 function is_weight_masked(model)
     sum(model.fs[1].weight[:, 2, 2]) == 0f0  # diagonal weight (self loop) is zero
@@ -44,6 +32,18 @@ function mask_weight!(model::CausalPGM)
     for i = 1:size(dag, 1)  # loop fcm i
         fs[1][:, :, i] .*= dag[:, i]  # mask input layer only
     end
+end
+
+""" Sample zero mean noises from CausalPGM.ps """
+function sample_noise(model::CausalPGM, n_samples::Int)
+    @> rand.(model.ps, n_samples) transpose.() vcats
+end
+
+""" One-step mapping throught the model, return the location """
+function (model::CausalPGM)(x::AbstractVecOrMat{<:Real})
+    @unpack dag, ps, fs = model
+    xb = @> unsqueeze(x, 2) repeat(outer=(1, size(dag, 1)))
+    yb = @> fs(xb) squeeze(1)
 end
 
 """ Forward the noises throught the model following the dag topo-order
