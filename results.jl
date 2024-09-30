bc19427894ca1e953e5f5a74939d587976baf00a
Phuoc Nguyen <phuoc.nguyen@deakin.edu.au>
2024-10-01 09:46:18 +1000

    16gpu-1

--- results.jl
+++ results.jl
@@ -41,7 +41,7 @@ sort!(rs, :method)
 #-- defaults
 # default(; fontfamily="Computer Modern", titlefontsize=14, linewidth=2, framestyle=:box, label=nothing, aspect_ratio=:equal, grid=true, xlim, ylim, zlim, color=:seaborn_deep, markersize=2, leg=nothing)
 default(; grid=true, markersize=2, markerstrokewidth=0, lw=2)
-plot(ylim=(0, 1), size=(600, 400))
+plot(ylim=(0, 1), size=(600, 400), xlab="k", ylab="NDCG@k")
 # plots linegraphs
 for i = 1:nrow(rs)
     plot!(rs[i, :mean], yerror=rs[i, :std] ./2 , lab = rs[i, :method])
